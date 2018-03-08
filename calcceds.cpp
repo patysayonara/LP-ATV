353fdd55a4b72e18fdd00b141cd3399bae10aaa9
Patricia Sayonara Goes de Araujo <patysayonara@A307-36L.imd.local>
2018-03-08 17:36:41 -0300

    Programa pronto!

---
+++ calcceds.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include "milionario.hpp"
+using std::cin;
+using std::cout;
+using std::endl;
+
+void calcCeds(){
+	int v[7], i, valor;
+	cout << "Digite o valor a ser analisado: ";
+	cin >> valor;
+	cout << "Os valores serao impressos em ordem de cédulas 100, 50, 20, 10, 5, 2 e 1" << endl;
+
+	for(i = 0; i < 7; i++){
+		v[i] = 0;
+	}
+
+	while(valor >= 100){
+		v[0]++;
+		valor -= 100;
+	}
+
+	while(valor >= 50){
+		v[1]++;
+		valor -= 50;
+	}
+
+	while(valor >= 20){
+		v[2]++;
+		valor -= 20;
+	}
+
+	while(valor >= 10){
+		v[3]++;
+		valor -= 10;
+	}
+
+	while(valor >= 5){
+		v[4]++;
+		valor -= 5;
+	}
+
+	while(valor >= 2){
+		v[5]++;
+		valor -= 2;
+	}
+
+	while(valor >= 1){
+		v[6]++;
+		valor -= 1;
+	}
+
+    for(i = 0; i < 7; i++) {
+        cout << v[i] << " cedula(s)." << endl;
+    }
+}
---
+++ vetor.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include "milionario.hpp"
+using std::cout;
+using std::cin;
+using std::endl;
+
+void vetor(){
+    int x, y, contador, novo, valor;
+    int vetor_1[10][2] ={{0}, {0}};
+
+    contador = 0;
+
+    cout << "Digite 10 numeros" << endl;
+
+    for (x = 0; x < 10; x++){
+        cin >> valor;
+
+        for (y = 0, novo = 1; y <= x; y++){
+            if (vetor_1[y][0] == valor){
+                vetor_1[y][1]++;
+                novo = 0;
+                y = x+1;
+            }
+        }
+
+        if (novo == 1){
+            vetor_1[contador][0] = valor;
+            vetor_1[contador][1]++;
+            contador++;
+        }
+    }
+
+
+    for (x = 0; x <= contador; x++){
+        if (vetor_1[x][1] >= 1){
+            cout << "O numero " << vetor_1[x][0] << " eh " << vetor_1[x][1] << endl;
+        }
+    }
+}
